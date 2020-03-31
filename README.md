0db94ccf1d98512e09ff3e42c26e2b4f5a491132
LyhourChhen <ukamilove66@gmail.com>
2020-03-31 17:01:12 +0700

    init .prettierc config & update with scalatype

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - [Demo Link](https://graphql-demo.mead.io/) - GraphQL Demo Online, Incase if you wanna try.
 - [GraphQL-Yoga](https://github.com/prisma-labs/graphql-yoga) - GraphQL Yoga-Documents
+
+Scala Types => `String, Boolean, Int, Float, ID`
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer } from 'graphql-yoga'
+import uuid from 'uuid'
 
 // Type Definition || Schema (This is where our structure should look like)
-// ! => always be return && if noting it will return null
+// ! => It ok not to be return && if noting it will return null
+
 const typeDefs = `
     type Query {
         hello: String!
@@ -9,25 +11,48 @@ const typeDefs = `
         location: String!
         bio: String
     }
-`;
+`
 
 // Resolvers => The function where where schema need to be performs
-const resolvers = {
-  //This must be match with type
-  Query: {
-    hello: () => "This is my First GraphQL Query",
-    name: () => "LyhourChhen",
-    location: () => "I love in PhnomPenh, City of 🇰🇭",
-    bio: () =>
-      "My name is LyhourChhen and i am also the software engineer who currently working with Frontend-Developments"
-  }
-};
 
+const resolvers = {
+    //This must be match with type
+    Query: {
+        hello: () => 'This is my First GraphQL Query',
+        name: () => 'LyhourChhen',
+        location: () => 'I love in PhnomPenh, City of 🇰🇭',
+        bio: () =>
+            'My name is LyhourChhen and i am also the software engineer who currently working with Frontend-Developments',
+    },
+}
+
+// With Scala Type => Boolean, String, ID, Int and Float
+
+const typeDefs_with_scalaType = `
+    type Query {
+        id: ID!
+        name: String!
+        age: Int!
+        employment: Boolean!
+        salary: Float
+    }
+`
+const resolvers_with_scalaType = {
+    Query: {
+        id: () => uuid(),
+        name: () => 'LyhourChhen',
+        age: () => 19,
+        employment: () => true,
+        salary: () => 333.33,
+    },
+}
+
+// -------------------------------
 const server = new GraphQLServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers
-});
+    typeDefs: [typeDefs_with_scalaType],
+    resolvers: [resolvers_with_scalaType],
+})
 
 server.start(() => {
-  console.log("Server is up !");
-});
+    console.log('Server is up !')
+})
