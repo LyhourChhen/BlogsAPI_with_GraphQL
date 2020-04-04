21fc12f35fdf4eef28f7ab14f81f62be525a8373
LyhourChhen <ukamilove66@gmail.com>
2020-04-04 16:36:05 +0700

    finally fixing bug

--- Prisma_GraphQL/src/prisma.js
+++ Prisma_GraphQL/src/prisma.js
@@ -2,5 +2,16 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466',
+    endpoint: 'http://localhost:4466',
 })
+
+// There are some common method to use in this project !
+// - prisma.query
+// - prisma.mutation
+// - prisma.subscription
+// - prisma.exist => some utility relate with graphql
+
+// test
+prisma.query
+    .users(null, '{id name email}')
+    .then((data) => console.log('return data', data))
