8241eaaea115db1e625da26199f8fd39c9231cc4
LyhourChhen <ukamilove66@gmail.com>
2020-04-04 17:08:34 +0700

    add mutation binding

--- Prisma_GraphQL/src/prisma.js
+++ Prisma_GraphQL/src/prisma.js
@@ -11,7 +11,33 @@ const prisma = new Prisma({
 // - prisma.subscription
 // - prisma.exist => some utility relate with graphql
 
-// test
-prisma.query
-    .users(null, '{id name email}')
-    .then((data) => console.log('return data', data))
+// ==> test
+
+// prisma.query
+//     .users(null, '{id name email}')
+//     .then((data) => console.log('return data', data))
+
+// prisma.query.comments(null, '{id text author {id name}}').then((comments) => {
+//     console.log(JSON.stringify(comments, null, 2))
+// })
+
+// mutation
+prisma.mutation
+    .createPost(
+        {
+            data: {
+                title: 'create post via prisma binding',
+                body: 'This is how i am binding via prisma',
+                published: true,
+                author: {
+                    connect: {
+                        id: 'ck8l5p9ii006i07044xqxxhmv',
+                    },
+                },
+            },
+        },
+        '{id title body published}',
+    )
+    .then((post) => {
+        console.log(JSON.stringify(post, null, 2))
+    })
